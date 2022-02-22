c6a9d7e5ca641cc02d0dd963e4c19323b9f52811
moto999999 <juan1999l@gmail.com>
2022-02-22 17:49:57 +0100

    Chip8.cpp: Load fontset at the start of the memory

--- Chip 8/Chip8.cpp
+++ Chip 8/Chip8.cpp
@@ -29,7 +29,7 @@ void Chip8::initialize()
 
 	// Load fontset
 	for (int i = 0; i < 80; i++)
-		memory[0x50 + i] = chip8_fontset[i];
+		memory[i] = chip8_fontset[i];
 
 	// Reset timers
 	delay_timer = 0;
@@ -249,8 +249,8 @@ void Chip8::emulateCycle()
 	case 0xD000:
 		/* 0xDXYN:
 		 * Draws a sprite at coordinate (VX, VY) that has a width of 8 pixels and a height of N pixels.
-		 * Each row of 8 pixels is read as bit-coded starting from memory location I; I value doesn’t change after the execution of this instruction.
-		 * As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that doesn’t happen
+		 * Each row of 8 pixels is read as bit-coded starting from memory location I; I value doesnï¿½t change after the execution of this instruction.
+		 * As described above, VF is set to 1 if any screen pixels are flipped from set to unset when the sprite is drawn, and to 0 if that doesnï¿½t happen
 		*/
 
 	{
